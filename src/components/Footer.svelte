58d1a86e485e9713466cb2205acb270fc9e98c7c
harsh tpss <harshktpss@gmail.com>
2021-04-20 15:20:13 +0530

    First Commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Container from "sveltestrap/src/Container.svelte";
+  </script>
+  
+  <footer class="py-4 bg-light mt-auto">
+    <Container fluid={true}>
+      <div class="d-flex align-items-center justify-content-between small">
+        <div class="text-muted">
+          Made with love by
+          <a href=".">Adarsh</a>
+          &
+          <a href=".">Prasad</a>
+          at
+          <a href=".">******</a>
+        </div>
+        <div>
+          <a href=".">Privacy Policy</a>
+          &middot;
+          <a href=".">Terms &amp; Conditions</a>
+        </div>
+      </div>
+    </Container>
+  </footer>
---
+++ src/routes/_layout.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Container from 'sveltestrap/src/Container.svelte';
+	import Navbar from "../components/Navbar.svelte";
+	import Footer from '../components/Footer.svelte';
+	import Sidebar from '../components/Sidebar.svelte';
+
+	let theme = "dark";
+	let color = "dark";
+	let title = "Admin Svelte";
+
+	export let segment;
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+{#if segment !== "pages"}
+	<div class="sb-nav-fixed">
+		<Navbar {segment} {color} {title} />
+		<div id="layoutSidenav">
+			<Sidebar {segment} {theme} />
+			<div id="layoutSidenav_content">
+				<main>
+					<Container fluid={true}>
+						<slot />
+					</Container>
+				</main>
+				<Footer />
+			</div>
+		</div>
+	</div>
+{:else}
+	<body>
+		<slot />
+	</body>
+{/if}
